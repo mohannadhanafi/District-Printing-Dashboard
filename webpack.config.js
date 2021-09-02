f8d5ad9a9d061a22efaa0c70b0f54a1c316ec4e1
mohannadhanafi <mohannad.hanafi96@gmail.com>
2021-09-02 20:14:13 +0200

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ const mainConfig = function () {
 		module: {
 			rules: [
 				{
-					test: /\.scss$/,
+					test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000',
 					use: [
 						MiniCssExtractPlugin.loader,
 						"css-loader",
